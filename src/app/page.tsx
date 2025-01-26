261c47b531e4d0591f9c77590f845e6ae4107546
David <dcoromina98@gmail.com>
2025-01-26 21:21:07 +0100

    clerk comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,40 @@ import Navbar from "../components/navbar";
 import HeroSection from "../components/Hero";
 import Footer from "../components/footer";
 import { BentoDemo } from "@/components/bento";
+import ClerkComponent from "@/components/clerk";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between w-full h-auto space-y-10">
-      <header className=" w-full h-1/6  space-y-10">
-        <title></title>
-      </header>
-      <body className="w-full">
-        <div id="navSection">
-          {" "}
-          <Navbar />
-        </div>
-        <HeroSection />
-        <BentoDemo />
+    <>
+      {/* Metadata */}
+      <Head>
+        <title>SolaTech</title>
+        <meta
+          name="Sola Technologies"
+          content="Website for a health and fitness company"
+        />
+      </Head>
+
+      {/* Main Content */}
+      <main className="flex flex-col items-center justify-between w-full h-auto space-y-10 p-3">
+        {/* Header Section */}
+        <header className="w-full h-auto space-y-10">
+          <div id="navSection">
+            <Navbar />
+          </div>
+        </header>
+
+        {/* Main Body Section */}
+        <section className="w-full">
+          <HeroSection />
+          <ClerkComponent />
+          <BentoDemo />
+        </section>
+
+        {/* Footer */}
         <Footer />
-      </body>
-    </main>
+      </main>
+    </>
   );
 }
